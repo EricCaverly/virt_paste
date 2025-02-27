82881a1647eb4480f254b216a3e0258b290f4b97
Eric Caverly <eric@ericc.ninja>
2025-02-26 19:49:04 -0700

    Implemented basic UI and Hotkey binding. Created empty class file for virtual typer

--- VirtPaste/VirtPaste/Form1.Designer.cs
+++ VirtPaste/VirtPaste/Form1.Designer.cs
@@ -23,13 +23,262 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.UI_GB_Compat = new System.Windows.Forms.GroupBox();
+            this.UI_PB_Progress = new System.Windows.Forms.ProgressBar();
+            this.UI_BTN_Stop = new System.Windows.Forms.Button();
+            this.UI_BTN_Hide = new System.Windows.Forms.Button();
+            this.UI_CB_RemoveReturn = new System.Windows.Forms.CheckBox();
+            this.UI_CB_RemoveCapslock = new System.Windows.Forms.CheckBox();
+            this.UI_CB_SendBackspaces = new System.Windows.Forms.CheckBox();
+            this.UI_TRB_Delay = new System.Windows.Forms.TrackBar();
+            this.UI_LBL_DelayMin = new System.Windows.Forms.Label();
+            this.UI_LBL_DelayMax = new System.Windows.Forms.Label();
+            this.UI_LBL_Delay = new System.Windows.Forms.Label();
+            this.UI_CB_Active = new System.Windows.Forms.CheckBox();
+            this.UI_GB_Hotkey = new System.Windows.Forms.GroupBox();
+            this.UI_CB_MOD_Ctrl = new System.Windows.Forms.CheckBox();
+            this.UI_CB_MOD_Alt = new System.Windows.Forms.CheckBox();
+            this.UI_CB_MOD_Shift = new System.Windows.Forms.CheckBox();
+            this.UI_CB_MOD_Win = new System.Windows.Forms.CheckBox();
+            this.UI_TB_Hotkey = new System.Windows.Forms.TextBox();
+            this.UI_GB_Compat.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_TRB_Delay)).BeginInit();
+            this.UI_GB_Hotkey.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UI_GB_Compat
+            // 
+            this.UI_GB_Compat.Controls.Add(this.UI_LBL_Delay);
+            this.UI_GB_Compat.Controls.Add(this.UI_LBL_DelayMax);
+            this.UI_GB_Compat.Controls.Add(this.UI_LBL_DelayMin);
+            this.UI_GB_Compat.Controls.Add(this.UI_TRB_Delay);
+            this.UI_GB_Compat.Controls.Add(this.UI_CB_SendBackspaces);
+            this.UI_GB_Compat.Controls.Add(this.UI_CB_RemoveCapslock);
+            this.UI_GB_Compat.Controls.Add(this.UI_CB_RemoveReturn);
+            this.UI_GB_Compat.Location = new System.Drawing.Point(12, 67);
+            this.UI_GB_Compat.Name = "UI_GB_Compat";
+            this.UI_GB_Compat.Size = new System.Drawing.Size(347, 97);
+            this.UI_GB_Compat.TabIndex = 0;
+            this.UI_GB_Compat.TabStop = false;
+            this.UI_GB_Compat.Text = "Compatibility Options";
+            // 
+            // UI_PB_Progress
+            // 
+            this.UI_PB_Progress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_PB_Progress.Location = new System.Drawing.Point(12, 181);
+            this.UI_PB_Progress.Name = "UI_PB_Progress";
+            this.UI_PB_Progress.Size = new System.Drawing.Size(347, 23);
+            this.UI_PB_Progress.TabIndex = 1;
+            // 
+            // UI_BTN_Stop
+            // 
+            this.UI_BTN_Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_BTN_Stop.Enabled = false;
+            this.UI_BTN_Stop.Location = new System.Drawing.Point(203, 210);
+            this.UI_BTN_Stop.Name = "UI_BTN_Stop";
+            this.UI_BTN_Stop.Size = new System.Drawing.Size(75, 23);
+            this.UI_BTN_Stop.TabIndex = 2;
+            this.UI_BTN_Stop.Text = "Stop";
+            this.UI_BTN_Stop.UseVisualStyleBackColor = true;
+            // 
+            // UI_BTN_Hide
+            // 
+            this.UI_BTN_Hide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_BTN_Hide.Location = new System.Drawing.Point(284, 210);
+            this.UI_BTN_Hide.Name = "UI_BTN_Hide";
+            this.UI_BTN_Hide.Size = new System.Drawing.Size(75, 23);
+            this.UI_BTN_Hide.TabIndex = 3;
+            this.UI_BTN_Hide.Text = "Hide";
+            this.UI_BTN_Hide.UseVisualStyleBackColor = true;
+            // 
+            // UI_CB_RemoveReturn
+            // 
+            this.UI_CB_RemoveReturn.AutoSize = true;
+            this.UI_CB_RemoveReturn.Location = new System.Drawing.Point(6, 19);
+            this.UI_CB_RemoveReturn.Name = "UI_CB_RemoveReturn";
+            this.UI_CB_RemoveReturn.Size = new System.Drawing.Size(77, 17);
+            this.UI_CB_RemoveReturn.TabIndex = 0;
+            this.UI_CB_RemoveReturn.Text = "Remove \\r";
+            this.UI_CB_RemoveReturn.UseVisualStyleBackColor = true;
+            // 
+            // UI_CB_RemoveCapslock
+            // 
+            this.UI_CB_RemoveCapslock.AutoSize = true;
+            this.UI_CB_RemoveCapslock.Location = new System.Drawing.Point(89, 19);
+            this.UI_CB_RemoveCapslock.Name = "UI_CB_RemoveCapslock";
+            this.UI_CB_RemoveCapslock.Size = new System.Drawing.Size(113, 17);
+            this.UI_CB_RemoveCapslock.TabIndex = 1;
+            this.UI_CB_RemoveCapslock.Text = "Override Capslock";
+            this.UI_CB_RemoveCapslock.UseVisualStyleBackColor = true;
+            // 
+            // UI_CB_SendBackspaces
+            // 
+            this.UI_CB_SendBackspaces.AutoSize = true;
+            this.UI_CB_SendBackspaces.Location = new System.Drawing.Point(208, 19);
+            this.UI_CB_SendBackspaces.Name = "UI_CB_SendBackspaces";
+            this.UI_CB_SendBackspaces.Size = new System.Drawing.Size(113, 17);
+            this.UI_CB_SendBackspaces.TabIndex = 2;
+            this.UI_CB_SendBackspaces.Text = "Send Backspaces";
+            this.UI_CB_SendBackspaces.UseVisualStyleBackColor = true;
+            // 
+            // UI_TRB_Delay
+            // 
+            this.UI_TRB_Delay.LargeChange = 10;
+            this.UI_TRB_Delay.Location = new System.Drawing.Point(6, 42);
+            this.UI_TRB_Delay.Maximum = 200;
+            this.UI_TRB_Delay.Minimum = 20;
+            this.UI_TRB_Delay.Name = "UI_TRB_Delay";
+            this.UI_TRB_Delay.Size = new System.Drawing.Size(335, 45);
+            this.UI_TRB_Delay.SmallChange = 5;
+            this.UI_TRB_Delay.TabIndex = 3;
+            this.UI_TRB_Delay.TickFrequency = 10;
+            this.UI_TRB_Delay.Value = 20;
+            // 
+            // UI_LBL_DelayMin
+            // 
+            this.UI_LBL_DelayMin.AutoSize = true;
+            this.UI_LBL_DelayMin.Location = new System.Drawing.Point(6, 74);
+            this.UI_LBL_DelayMin.Name = "UI_LBL_DelayMin";
+            this.UI_LBL_DelayMin.Size = new System.Drawing.Size(32, 13);
+            this.UI_LBL_DelayMin.TabIndex = 4;
+            this.UI_LBL_DelayMin.Text = "20ms";
+            // 
+            // UI_LBL_DelayMax
+            // 
+            this.UI_LBL_DelayMax.AutoSize = true;
+            this.UI_LBL_DelayMax.Location = new System.Drawing.Point(309, 74);
+            this.UI_LBL_DelayMax.Name = "UI_LBL_DelayMax";
+            this.UI_LBL_DelayMax.Size = new System.Drawing.Size(38, 13);
+            this.UI_LBL_DelayMax.TabIndex = 5;
+            this.UI_LBL_DelayMax.Text = "200ms";
+            // 
+            // UI_LBL_Delay
+            // 
+            this.UI_LBL_Delay.Location = new System.Drawing.Point(44, 74);
+            this.UI_LBL_Delay.Name = "UI_LBL_Delay";
+            this.UI_LBL_Delay.Size = new System.Drawing.Size(259, 13);
+            this.UI_LBL_Delay.TabIndex = 6;
+            this.UI_LBL_Delay.Text = "Delay";
+            this.UI_LBL_Delay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UI_CB_Active
+            // 
+            this.UI_CB_Active.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.UI_CB_Active.AutoSize = true;
+            this.UI_CB_Active.Location = new System.Drawing.Point(12, 214);
+            this.UI_CB_Active.Name = "UI_CB_Active";
+            this.UI_CB_Active.Size = new System.Drawing.Size(56, 17);
+            this.UI_CB_Active.TabIndex = 4;
+            this.UI_CB_Active.Text = "Active";
+            this.UI_CB_Active.UseVisualStyleBackColor = true;
+            // 
+            // UI_GB_Hotkey
+            // 
+            this.UI_GB_Hotkey.Controls.Add(this.UI_TB_Hotkey);
+            this.UI_GB_Hotkey.Controls.Add(this.UI_CB_MOD_Win);
+            this.UI_GB_Hotkey.Controls.Add(this.UI_CB_MOD_Shift);
+            this.UI_GB_Hotkey.Controls.Add(this.UI_CB_MOD_Alt);
+            this.UI_GB_Hotkey.Controls.Add(this.UI_CB_MOD_Ctrl);
+            this.UI_GB_Hotkey.Location = new System.Drawing.Point(12, 12);
+            this.UI_GB_Hotkey.Name = "UI_GB_Hotkey";
+            this.UI_GB_Hotkey.Size = new System.Drawing.Size(347, 49);
+            this.UI_GB_Hotkey.TabIndex = 5;
+            this.UI_GB_Hotkey.TabStop = false;
+            this.UI_GB_Hotkey.Text = "Hotkey";
+            // 
+            // UI_CB_MOD_Ctrl
+            // 
+            this.UI_CB_MOD_Ctrl.AutoSize = true;
+            this.UI_CB_MOD_Ctrl.Location = new System.Drawing.Point(9, 19);
+            this.UI_CB_MOD_Ctrl.Name = "UI_CB_MOD_Ctrl";
+            this.UI_CB_MOD_Ctrl.Size = new System.Drawing.Size(54, 17);
+            this.UI_CB_MOD_Ctrl.TabIndex = 0;
+            this.UI_CB_MOD_Ctrl.Text = "CTRL";
+            this.UI_CB_MOD_Ctrl.UseVisualStyleBackColor = true;
+            // 
+            // UI_CB_MOD_Alt
+            // 
+            this.UI_CB_MOD_Alt.AutoSize = true;
+            this.UI_CB_MOD_Alt.Location = new System.Drawing.Point(69, 19);
+            this.UI_CB_MOD_Alt.Name = "UI_CB_MOD_Alt";
+            this.UI_CB_MOD_Alt.Size = new System.Drawing.Size(46, 17);
+            this.UI_CB_MOD_Alt.TabIndex = 1;
+            this.UI_CB_MOD_Alt.Text = "ALT";
+            this.UI_CB_MOD_Alt.UseVisualStyleBackColor = true;
+            // 
+            // UI_CB_MOD_Shift
+            // 
+            this.UI_CB_MOD_Shift.AutoSize = true;
+            this.UI_CB_MOD_Shift.Location = new System.Drawing.Point(121, 19);
+            this.UI_CB_MOD_Shift.Name = "UI_CB_MOD_Shift";
+            this.UI_CB_MOD_Shift.Size = new System.Drawing.Size(57, 17);
+            this.UI_CB_MOD_Shift.TabIndex = 2;
+            this.UI_CB_MOD_Shift.Text = "SHIFT";
+            this.UI_CB_MOD_Shift.UseVisualStyleBackColor = true;
+            // 
+            // UI_CB_MOD_Win
+            // 
+            this.UI_CB_MOD_Win.AutoSize = true;
+            this.UI_CB_MOD_Win.Location = new System.Drawing.Point(184, 19);
+            this.UI_CB_MOD_Win.Name = "UI_CB_MOD_Win";
+            this.UI_CB_MOD_Win.Size = new System.Drawing.Size(48, 17);
+            this.UI_CB_MOD_Win.TabIndex = 3;
+            this.UI_CB_MOD_Win.Text = "WIN";
+            this.UI_CB_MOD_Win.UseVisualStyleBackColor = true;
+            // 
+            // UI_TB_Hotkey
+            // 
+            this.UI_TB_Hotkey.Location = new System.Drawing.Point(238, 17);
+            this.UI_TB_Hotkey.MaxLength = 1;
+            this.UI_TB_Hotkey.Name = "UI_TB_Hotkey";
+            this.UI_TB_Hotkey.Size = new System.Drawing.Size(28, 20);
+            this.UI_TB_Hotkey.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(371, 245);
+            this.Controls.Add(this.UI_GB_Hotkey);
+            this.Controls.Add(this.UI_CB_Active);
+            this.Controls.Add(this.UI_BTN_Hide);
+            this.Controls.Add(this.UI_BTN_Stop);
+            this.Controls.Add(this.UI_PB_Progress);
+            this.Controls.Add(this.UI_GB_Compat);
+            this.Name = "Form1";
+            this.Text = "Virt Paste";
+            this.UI_GB_Compat.ResumeLayout(false);
+            this.UI_GB_Compat.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_TRB_Delay)).EndInit();
+            this.UI_GB_Hotkey.ResumeLayout(false);
+            this.UI_GB_Hotkey.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox UI_GB_Compat;
+        private System.Windows.Forms.CheckBox UI_CB_SendBackspaces;
+        private System.Windows.Forms.CheckBox UI_CB_RemoveCapslock;
+        private System.Windows.Forms.CheckBox UI_CB_RemoveReturn;
+        private System.Windows.Forms.ProgressBar UI_PB_Progress;
+        private System.Windows.Forms.Button UI_BTN_Stop;
+        private System.Windows.Forms.Button UI_BTN_Hide;
+        private System.Windows.Forms.TrackBar UI_TRB_Delay;
+        private System.Windows.Forms.Label UI_LBL_DelayMin;
+        private System.Windows.Forms.Label UI_LBL_Delay;
+        private System.Windows.Forms.Label UI_LBL_DelayMax;
+        private System.Windows.Forms.CheckBox UI_CB_Active;
+        private System.Windows.Forms.GroupBox UI_GB_Hotkey;
+        private System.Windows.Forms.CheckBox UI_CB_MOD_Win;
+        private System.Windows.Forms.CheckBox UI_CB_MOD_Shift;
+        private System.Windows.Forms.CheckBox UI_CB_MOD_Alt;
+        private System.Windows.Forms.CheckBox UI_CB_MOD_Ctrl;
+        private System.Windows.Forms.TextBox UI_TB_Hotkey;
     }
 }
 
--- VirtPaste/VirtPaste/Form1.cs
+++ VirtPaste/VirtPaste/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,203 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VirtPaste {
+
+    public enum KeyModifier {       // Used to register hotkeys
+        None = 0,
+        Alt = 1,
+        Ctrl = 2,
+        Shift = 4,
+        Super = 8
+    }
+
+    public struct Settings {        // Stores active session information
+        public bool active;
+        public bool removeReturn;
+        public bool overrideCaps;
+        public bool sendBackspaces;
+        public int delayMs;
+        public Keys hotkeyKey;
+        public short hotkeyModifiers;
+    }
+
     public partial class Form1 : Form {
+        // IDs for registered hotkeys
+        const int PASTE_HOTKEY_ID = 1;
+        const int CANCEL_HOTKEY_ID = 2;
+
+        /* Win32 Register Hotkey prototypes */
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern short VkKeyScan(char ch);
+
+        // Default | Class global settings
+        public Settings settings = new Settings {
+            active = true,
+            removeReturn = true,
+            overrideCaps = true,
+            sendBackspaces = false,
+            delayMs = 20,
+            hotkeyModifiers = (short)(KeyModifier.Ctrl | KeyModifier.Shift),
+            hotkeyKey = Keys.V
+        };
+
+
+        // Form constructor
         public Form1() {
             InitializeComponent();
+
+            // Make UI reflect default settings
+            SettingsToUI();
+
+            // Bind default hotkey
+            BindHotkey();
+            RegisterHotKey(Handle, CANCEL_HOTKEY_ID, 0, (int)Keys.Escape);
+
+            // Bind event handlers for changes to UI for hotkey
+            UI_CB_MOD_Alt.CheckedChanged += HotkeyChangedEvent;
+            UI_CB_MOD_Ctrl.CheckedChanged += HotkeyChangedEvent;
+            UI_CB_MOD_Shift.CheckedChanged += HotkeyChangedEvent;
+            UI_CB_MOD_Win.CheckedChanged += HotkeyChangedEvent;
+            UI_TB_Hotkey.TextChanged += HotkeyChangedEvent;
+
+            // Bind active checkbox handler
+            UI_CB_Active.CheckedChanged += UI_CB_Active_CheckedChanged;
+
+            // Bind other settings handlers
+            UI_CB_RemoveCapslock.CheckedChanged += CompatChangedEvent;
+            UI_CB_RemoveReturn.CheckedChanged += CompatChangedEvent;
+            UI_CB_SendBackspaces.CheckedChanged += CompatChangedEvent;
+            UI_TRB_Delay.ValueChanged += CompatChangedEvent;
+        }
+
+
+        /*****************************
+         * Method  : HotkeyChangedEvent
+         * Purpose : Consolidated event handler. Detect when the UI has been interected with on any hotkey settings. Rebind hotkeys with new settings
+         *****************************/
+        private void HotkeyChangedEvent(object sender, EventArgs e) {
+            UnregisterHotKey(Handle, PASTE_HOTKEY_ID);
+            UnregisterHotKey(Handle, CANCEL_HOTKEY_ID);
+
+            // Ensure a character is present in key selection box
+            if (UI_TB_Hotkey.Text.Length == 0) {
+                MessageBox.Show("You must have a key specified");
+                return;
+            }
+
+            // Get the key in the key selection box
+            settings.hotkeyKey = (Keys)char.ToUpper(UI_TB_Hotkey.Text[0]);
+
+            settings.hotkeyModifiers = 0;
+            settings.hotkeyModifiers |= (short)(UI_CB_MOD_Alt.Checked ? KeyModifier.Alt : 0);
+            settings.hotkeyModifiers |= (short)(UI_CB_MOD_Ctrl.Checked ? KeyModifier.Ctrl : 0);
+            settings.hotkeyModifiers |= (short)(UI_CB_MOD_Shift.Checked ? KeyModifier.Shift : 0);
+            settings.hotkeyModifiers |= (short)(UI_CB_MOD_Win.Checked ? KeyModifier.Super : 0);
+
+            // Ensure there is at least 1 modifier
+            if (settings.hotkeyModifiers == 0) {
+                MessageBox.Show("Must have at least 1 modifier selected");
+                return;
+            }
+
+            //Trace.WriteLine($"Key: {(Keys)settings.hotkeyKey} | Modifiers : {settings.hotkeyModifiers}");
+
+            BindHotkey();
+            RegisterHotKey(Handle, CANCEL_HOTKEY_ID, 0, (int)Keys.Escape);
+        }
+
+
+        /**************************
+         * Method  : CompatChangedEvent
+         * Purpose : Change settings based on UI changes
+         *************************/
+        private void CompatChangedEvent(object sender, EventArgs e) {
+            settings.delayMs = UI_TRB_Delay.Value;
+            settings.overrideCaps = UI_CB_RemoveCapslock.Checked;
+            settings.removeReturn = UI_CB_RemoveReturn.Checked;
+            settings.sendBackspaces = UI_CB_SendBackspaces.Checked;
+        }
+
+
+        /**************************
+         * Method  : UI_CB_Active_CheckedChanged
+         * Purpose : Toggle functionality based on active checkbox
+         **************************/
+        private void UI_CB_Active_CheckedChanged(object sender, EventArgs e) {
+            settings.active = UI_CB_Active.Checked;
+        }
+
+
+        /*****************************
+         * Method  : SettingsToUI
+         * Purpose : Takes a settings argument and sets the WinForms UI to reflect them
+         * Params  :
+         *   s -> Settings to show on UI
+         ****************************/
+        internal void SettingsToUI() {
+            UI_CB_RemoveCapslock.Checked = settings.overrideCaps;
+            UI_CB_RemoveReturn.Checked = settings.removeReturn;
+            UI_CB_SendBackspaces.Checked = settings.sendBackspaces;
+            UI_TRB_Delay.Value = settings.delayMs;
+            if ((settings.hotkeyModifiers & (short)KeyModifier.Alt) != 0)
+                UI_CB_MOD_Alt.Checked = true;
+            if ((settings.hotkeyModifiers & (short)KeyModifier.Shift) != 0)
+                UI_CB_MOD_Shift.Checked = true;
+            if ((settings.hotkeyModifiers & (short)KeyModifier.Ctrl) != 0)
+                UI_CB_MOD_Ctrl.Checked = true;
+            if ((settings.hotkeyModifiers & (short)KeyModifier.Super) != 0)
+                UI_CB_MOD_Win.Checked = true;
+            UI_TB_Hotkey.Text = ((Keys)settings.hotkeyKey).ToString();
+            UI_CB_Active.Checked = settings.active;
+        }
+
+
+        /******************************
+         * Method  : BindHotkey
+         * Purpose : Given the passed settings, attempt to bind a system wide hotkey using Win32.h
+         *****************************/
+        internal void BindHotkey() {            
+            // Register the hotkey
+            if(!RegisterHotKey(Handle, PASTE_HOTKEY_ID, settings.hotkeyModifiers, (int)settings.hotkeyKey)) {
+                MessageBox.Show("Failed to bind hotkey");
+                return;
+            }
+        }
+
+
+        /**********************************
+         * Method  : WndProc
+         * Purpose : Recieve windows events. Used to check if hotkey was pressed
+         * Params  : 
+         *   m -> message sent from windows
+         **********************************/
+        protected override void WndProc(ref Message m) {
+            // Perform standard WinForms processing of events
+            base.WndProc(ref m);
+
+            // Check if the event is "hotkey" and the tool is enabled
+            if (m.Msg == 0x0312 && settings.active) {
+
+                // Obtain the ID of the event
+                int id = m.WParam.ToInt32();
+
+                //Trace.WriteLine($"Hotkey pressed: {id}");
+
+                switch (id) {
+                    case PASTE_HOTKEY_ID:
+                        //if (!backgroundWorker1.IsBusy)
+                        //    start_typing();
+                        break;
+                    case CANCEL_HOTKEY_ID:
+                        //if (backgroundWorker1.IsBusy)
+                        //    stop_typing();
+                        break;
+                }
+
+            }
         }
     }
 }
